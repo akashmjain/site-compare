0607b3875c0195d6ad7bf5f24dae47b4ceedb249
harshitajain1811 <56118132+harshitajain1811@users.noreply.github.com>
2020-07-26 22:18:57 +0530

    Added styling

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -6,22 +6,28 @@
     </div>
 </template>
 <script>
+import 'bootstrap/dist/css/bootstrap.css';
 export default {
     
     name: "Header"
 }
 </script>
 
-<style scoped>
+<style>
     .header {
-        background: black;
-        color: white;
+        background: #343148ff;
+        color: #ffffff;
         text-align: center;
         padding: 10px;
+        font-size : 48px ;
+		font-style : italic;
+		font-variant : small-caps;
+		font-family : 'Arial','sans-serif';
+	}
+	@media only screen and (max-width: 479px)
+    {
+        .header {
+			font : 5px;
+		}
     }
-    .header a {
-        color: white;
-        padding-right: 5px;
-    }
-
 </style>
